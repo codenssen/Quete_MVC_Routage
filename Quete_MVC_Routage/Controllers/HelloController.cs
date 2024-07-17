9ae68943311405a8e03c5cfdac95e9e0b9d92bcf
ABT Aurelien <aurelien@abrassart.net>
2024-07-17 10:26:52 +0200

    Ajoutez des fichiers projet.

---
+++ Quete_MVC_Routage/Controllers/HelloController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Quete_MVC_Routage.Controllers
+{
+    public class HelloController : Controller
+    {
+        [Route("Say")]
+        public IActionResult SayHello()
+        {
+            return Content("Hello !");
+        }
+
+        [Route("Yell")]
+        public IActionResult YellHello()
+        {
+            return Content("HELLO !");
+        }
+
+     
+    }
+}
---
+++ Quete_MVC_Routage/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Quete_MVC_Routage.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
